728159912ffd385ff0f0f10666d8b3fc18093fa0
mararrjo <chemari70@gmail.com>
2014-06-02 11:11:16 +0200

    Crear partidas

--- src/Caos/MonopolyBundle/Controller/UsuarioController.php
+++ src/Caos/MonopolyBundle/Controller/UsuarioController.php
@@ -4,7 +4,6 @@ namespace Caos\MonopolyBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use Caos\MonopolyBundle\Entity\Usuario;
 use Caos\MonopolyBundle\Form\UsuarioType;
 
@@ -12,46 +11,44 @@ use Caos\MonopolyBundle\Form\UsuarioType;
  * Usuario controller.
  *
  */
-class UsuarioController extends Controller
-{
+class UsuarioController extends Controller {
 
-    public function loginAction(){
-        if($usuario){
+    public function loginAction() {
+        if ($usuario) {
             session_destroy();
             session_start();
             $_SESSION["usuario_id"] = $usuario->getId();
             $_SESSION["usuario_nombre"] = $usuario->getNombre();
         }
     }
-    
-    public function estaAutenticadoAction(){
-        if(isset($_SESSION["usuario_id"])){
-            return new \Symfony\Component\HttpFoundation\JsonResponse(array("autenticado"=>true));
-        }else{
-            return new \Symfony\Component\HttpFoundation\JsonResponse(array("autenticado"=>false));
+
+    public function estaAutenticadoAction() {
+        if (isset($_SESSION["usuario_id"])) {
+            return new \Symfony\Component\HttpFoundation\JsonResponse(array("autenticado" => true));
+        } else {
+            return new \Symfony\Component\HttpFoundation\JsonResponse(array("autenticado" => false));
         }
     }
-    
+
     /**
      * Lists all Usuario entities.
      *
      */
-    public function indexAction()
-    {
-        if($this->estaAutenticadoAction()){
-        $em = $this->getDoctrine()->getManager();
+    public function indexAction() {
+        if ($this->estaAutenticadoAction()) {
+            $em = $this->getDoctrine()->getManager();
 
-        $entities = $em->getRepository('CaosMonopolyBundle:Usuario')->obtenerTodos();
+            $entities = $em->getRepository('CaosMonopolyBundle:Usuario')->obtenerTodos();
 
-        return new \Symfony\Component\HttpFoundation\JsonResponse($entities);
+            return new \Symfony\Component\HttpFoundation\JsonResponse($entities);
         }
     }
+
     /**
      * Creates a new Usuario entity.
      *
      */
-    public function createAction(Request $request)
-    {
+    public function createAction(Request $request) {
         $entity = new Usuario();
         $form = $this->createCreateForm($entity);
         $form->handleRequest($request);
@@ -65,20 +62,19 @@ class UsuarioController extends Controller
         }
 
         return $this->render('CaosMonopolyBundle:Usuario:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));        
+                    'entity' => $entity,
+                    'form' => $form->createView(),
+        ));
     }
 
     /**
-    * Creates a form to create a Usuario entity.
-    *
-    * @param Usuario $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createCreateForm(Usuario $entity)
-    {
+     * Creates a form to create a Usuario entity.
+     *
+     * @param Usuario $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Usuario $entity) {
         $form = $this->createForm(new UsuarioType(), $entity, array(
             'action' => $this->generateUrl('usuario_create'),
             'method' => 'POST',
@@ -93,14 +89,13 @@ class UsuarioController extends Controller
      * Displays a form to create a new Usuario entity.
      *
      */
-    public function newAction()
-    {
+    public function newAction() {
         $entity = new Usuario();
-        $form   = $this->createCreateForm($entity);
+        $form = $this->createCreateForm($entity);
 
         return $this->render('CaosMonopolyBundle:Usuario:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
+                    'entity' => $entity,
+                    'form' => $form->createView(),
         ));
     }
 
@@ -108,8 +103,7 @@ class UsuarioController extends Controller
      * Finds and displays a Usuario entity.
      *
      */
-    public function showAction($id)
-    {
+    public function showAction($id) {
         $em = $this->getDoctrine()->getManager();
 
         $entity = $em->getRepository('CaosMonopolyBundle:Usuario')->obtenerPorId($id);
@@ -117,17 +111,15 @@ class UsuarioController extends Controller
         if (!$entity) {
             return new \Symfony\Component\HttpFoundation\JsonResponse("");
         }
-        
+
         return new \Symfony\Component\HttpFoundation\JsonResponse($entity[0]);
-        
     }
 
     /**
      * Displays a form to edit an existing Usuario entity.
      *
      */
-    public function editAction($id)
-    {
+    public function editAction($id) {
         $em = $this->getDoctrine()->getManager();
 
         $entity = $em->getRepository('CaosMonopolyBundle:Usuario')->find($id);
@@ -140,21 +132,20 @@ class UsuarioController extends Controller
         $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('CaosMonopolyBundle:Usuario:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+                    'entity' => $entity,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
         ));
     }
 
     /**
-    * Creates a form to edit a Usuario entity.
-    *
-    * @param Usuario $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Usuario $entity)
-    {
+     * Creates a form to edit a Usuario entity.
+     *
+     * @param Usuario $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createEditForm(Usuario $entity) {
         $form = $this->createForm(new UsuarioType(), $entity, array(
             'action' => $this->generateUrl('usuario_update', array('id' => $entity->getId())),
             'method' => 'PUT',
@@ -164,12 +155,12 @@ class UsuarioController extends Controller
 
         return $form;
     }
+
     /**
      * Edits an existing Usuario entity.
      *
      */
-    public function updateAction(Request $request, $id)
-    {
+    public function updateAction(Request $request, $id) {
         $em = $this->getDoctrine()->getManager();
 
         $entity = $em->getRepository('CaosMonopolyBundle:Usuario')->find($id);
@@ -189,17 +180,17 @@ class UsuarioController extends Controller
         }
 
         return $this->render('CaosMonopolyBundle:Usuario:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+                    'entity' => $entity,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
         ));
     }
+
     /**
      * Deletes a Usuario entity.
      *
      */
-    public function deleteAction(Request $request, $id)
-    {
+    public function deleteAction(Request $request, $id) {
         $form = $this->createDeleteForm($id);
         $form->handleRequest($request);
 
@@ -225,16 +216,15 @@ class UsuarioController extends Controller
      *
      * @return \Symfony\Component\Form\Form The form
      */
-    private function createDeleteForm($id)
-    {
+    private function createDeleteForm($id) {
         return $this->createFormBuilder()
-            ->setAction($this->generateUrl('usuario_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
+                        ->setAction($this->generateUrl('usuario_delete', array('id' => $id)))
+                        ->setMethod('DELETE')
+                        ->add('submit', 'submit', array('label' => 'Delete'))
+                        ->getForm()
         ;
     }
-    
+
     /**
      * Método propio que se encarga de crear un nuevo usuario en la BD
      *      Si el nombre de usuario ya se encuentra en la BD devuelve un mensaje de error
@@ -248,40 +238,48 @@ class UsuarioController extends Controller
         $nombre = $request->get("usuario");
         $password = $request->get("password");
         $email = $request->get("email");
-        
+
         // Comprobamos si el nombre de usuario ya está en la BD
         $em = $this->getDoctrine()->getManager();
         $usuario = $em->getRepository("CaosMonopolyBundle:Usuario")->obtenerPorNombre($nombre);
-                
-        if($usuario) { 
+
+        if ($usuario) {
             // Si el usuario ya existe devolvemos un mensaje de error
             return new \Symfony\Component\HttpFoundation\JsonResponse(array("tipo" => "error", "error" => "El nombre de usuario ya existe"));
-            
-        } else { 
+        } else {
             // Creamos un nuevo objeto del tipo "Caos\MonopolyBundle\Usuario"
             $nuevo = new Usuario();
-            
+
             // Le damos todos los datos que ha pasado el cliente por el formulario
             $nuevo->setNombre($nombre);
             $nuevo->setPassword(md5($password));
             $nuevo->setEmail($email);
             $nuevo->setEstado(true);
             $nuevo->setRol("ROLE_USER");
-            
+
             // Persistimos el objeto en la BD (ES NEESARIO AMBOS MÉTODOS)
             $em->persist($nuevo);
             $em->flush();
-            
+
             // Iniciar una sesión para el usuario
             session_destroy();
             session_start();
-            
+
             $_SESSION["id"] = $nuevo->getId();
             $_SESSION["user"] = $nuevo->getNombre();
-            
+
             // Devolvemos un mensaje al cliente
-            return new \Symfony\Component\HttpFoundation\JsonResponse(array("tipo" => "msg", "msg" => "El usuario se ha creado con éxito"));        
+            return new \Symfony\Component\HttpFoundation\JsonResponse(array("tipo" => "msg", "msg" => "El usuario se ha creado con éxito"));
         }
     }
-        
+
+    public function conectadosAction() {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('CaosMonopolyBundle:Usuario')->obtenerUsuariosConectados();
+
+        return new \Symfony\Component\HttpFoundation\JsonResponse($entities);
+    }
+
 }
